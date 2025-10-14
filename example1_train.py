670126fbb12e230c8ed00e79aee74b4de0a739b2
Muskan vaishnav <muskan25ai030@satiengg.in>
2025-10-14 12:59:13 +0000

    5th commit

--- example1_train.py
+++ example1_train.py
@@ -1,49 +1,59 @@
 import mlflow
+
 import mlflow.sklearn
+
+import numpy as np
+
 from sklearn.datasets import load_iris
+
 from sklearn.model_selection import train_test_split
+
 from sklearn.linear_model import LogisticRegression
+
 from sklearn.metrics import accuracy_score
 
-import mlflow.models
-from mlflow.models.signature import infer_signature
-
- 
 # Set up MLflow experiment
+
 mlflow.set_experiment("iris_classification")
  
-iris=load_iris()
-# print(iris)
-print(iris.data) #features
-print("********")
-print(iris.target) 
+iris = load_iris()
 
-x_train,x_test,y_train,y_test= train_test_split(
+# print(iris)
+
+print(iris.data)
+
+print(iris.target)
+ 
+x_train, x_test, y_train, y_test = train_test_split(
 
     iris.data,
+
     iris.target,
+
     test_size=0.2,
-    random_state=70
+
+    random_state=42
 
 )
 
-# print("x_train",x_train) 
-# print("y_train",y_train) 
-# print("x_test",x_test) 
-# print("y_test",y_test) 
+input_example = np.array([x_test[0]])
 
 with mlflow.start_run():
-    model= LogisticRegression(max_iter=200) #Train model
-    model.fit(x_train,y_train) #fir the model
 
+    model = LogisticRegression(max_iter=200)
 
-    preds = infer_signature(x_train, model.predict(x_test)) #make prediction and calculate accuracy
-    #remaning 30 samples 
-    #preds are the predicted tagets (0/1/2) for the test set
-    acc=accuracy_score(y_test,preds)
+    model.fit(x_train, y_train)
+ 
+    preds =  model.predict(x_test)
 
-    mlflow.log_param("Model_types","Logistic_regression")
-    mlflow.log_metric("accuracy",acc)
-    mlflow.sklearn.log_model(model,name="model")
+    acc = accuracy_score(y_test, preds)
+ 
+    mlflow.log_param("model_type", "logistic_regression")
 
-    print(f"run logged to Mlflow Accuracy:{acc:3f}")
+    mlflow.log_metric("accuracy", acc)
+
+    mlflow.sklearn.log_model(model, name="model", input_example=input_example)
+ 
+    print(f"Run loggedd to mlflow . Accuracy: {acc:.3f}")
+
+ 
